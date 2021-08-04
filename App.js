4e1d0f76f6e5e8825db8885744c7c1d0cf26ae81
Abu Zaid Khan <ak3776@drexel.edu>
2021-08-04 08:18:26 -0400

    heads and Tails

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+class Coin extends React.Component{
+  state = {
+    face: <Head/>,
+  }
+  render(){
+    const face = this.state.face;
+    return(
+      <View>
+        {face}
+        <Button
+        title="Flip"
+        onPress={this.onFlip}
+        />
+      </View>
+    );
+  }
+  onFlip = () =>{
+    var rand = Math.floor(Math.random() *100) + 1;
+    if(rand%2==0){
+      this.setState({face: <Head/>,})
+    }else{
+      this.setState({face: <Tail/>,})
+    }
+  }
+  
+}
+
+const Head = () =>{
+  return <View style={styles.head}><Text>H</Text></View>;
+}
+const Tail = () =>{
+  return <View style={styles.tail}><Text>T</Text></View>;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Coin/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +51,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  head: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    backgroundColor: "red",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tail: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    backgroundColor: "green",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
